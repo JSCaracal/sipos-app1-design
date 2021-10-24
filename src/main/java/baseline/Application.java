4c764347ac228ee5b2a330854f081bce5f659619
JSCaracal <siposj@knights.ucf.edu>
2021-10-24 18:36:51 -0400

    gui desing

---
+++ src/main/java/baseline/Application.java
@@ -0,0 +1,22 @@
+package baseline;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+public class Application extends javafx.application.Application
+{
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root =
+
+        FXMLLoader.load(getClass().getResource("TodoList.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Todo List");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/baseline/TodoListController.java
@@ -0,0 +1,31 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TodoListController {
+
+    @FXML
+    private Button LoadButton;
+
+    @FXML
+    private Button NewlistButton;
+
+    @FXML
+    private Button quitButton;
+
+    @FXML
+    void exitProgram(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initNewList(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadBttn(ActionEvent event) {
+
+    }
+
+}
